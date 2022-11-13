b560134c83073272fcf02ee3b3067664f07ec1bc
peterhuang <peterhuang@canway.net>
2022-11-13 20:21:17 +0800

    feature: init-code

---
+++ src/main/java/com/jvm/User.java
@@ -0,0 +1,32 @@
+package com.jvm;
+
+public class User {
+	
+	private int id;
+	private String name;
+
+	// 1024B * 100 = 100KB
+	byte[] a = new byte[1024*100];
+
+	public User(){}
+
+	public User(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ src/test/java/com/example/seckill/DynamicAgeTest.java
@@ -0,0 +1,28 @@
+package com.example.seckill;
+
+/**
+ * @author huangyizeng
+ * @description
+ * @date 2022/11/11
+ **/
+public class DynamicAgeTest {
+
+    private static final int _1MB = 1024 * 1024;
+
+    /***
+     * VM参数： -verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8 -XX:MaxTenuringThreshold=8
+     * @author huangyizeng 2022/11/11
+     * @param args
+     * java -Xms3072M -Xmx3072 -Xss1M -XX:MetaspaceSize=512M -XX:MaxMetaspaceSize=512M
+     */
+    public static void main(String[] args) {
+//        byte[] allocation1, allocation2, allocation3, allocation4;
+//        allocation1 = new byte[_1MB / 4];
+//        allocation2 = new byte[_1MB / 4];
+
+//        allocation3 = new byte[4 * _1MB];
+//        allocation4 = new byte[4 * _1MB];
+//        allocation4 = null;
+//        allocation4 = new byte[4 * _1MB];
+    }
+}
